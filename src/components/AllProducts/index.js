32689cd042a9abf9eb026dd92d8097f001853cb3
Patryk Sadza <exihuatl@icloud.com>
2018-08-08 10:55:08 +0200

    fix reducer

--- src/components/AllProducts/index.js
+++ src/components/AllProducts/index.js
@@ -1,38 +1,40 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { fetchItems } from '../../store/actions/index'
 
-import Item from '../../containers/Item';
+import Item from '../../containers/Item'
 
 export class AllProducts extends Component {
-
   componentWillMount() {
-    console.log('aaa', this.props.fetchItems())
     this.props.fetchItems()
   }
 
   render() {
-    console.log('data', this.props.data)
     return (
       <div>
-        {this.props.data && this.props.data.map((item, index) =>
-          <Item
-            key={index}
-            category={item.category}
-            name={item.name}
-            descripton={item.description}
-            url={item.url}
-          />
-        )}
+        {console.log('Mapowanie po obiekcie będzie tutaj', this.props.items)}
+        {/* {this.props.items &&
+          this.props.items.map((item, index) => (
+            <Item
+              key={index}
+              category={item.category}
+              name={item.name}
+              descripton={item.description}
+              url={item.url}
+            />
+          ))} */}
       </div>
     )
   }
 }
 
-const mapStateToProps = ({ data }) => data
+const mapStateToProps = ({ items }) => ({ items })
 
 const mapDispatchToProps = dispatch => ({
-  fetchItems: () => dispatch(fetchItems()),
+  fetchItems: () => dispatch(fetchItems())
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(AllProducts);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AllProducts)
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,16 @@
-import { combineReducers } from 'redux'
-import addItem from './addItem'
-import fetchItems from './fetchItems'
+import { ADD_ITEM, FETCH_ITEMS } from '../actions/index'
 
-export default combineReducers({
-  addItem,
-  fetchItems,
-})
+const initialState = {
+  items: {}
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ITEM:
+      return { ...state, items: [...state.items, action.payload] }
+    case FETCH_ITEMS:
+      return { ...state, items: action.payload }
+    default:
+      return state
+  }
+}
